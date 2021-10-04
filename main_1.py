a2a9a2bd7acc76d4e6a96f9847e8307811ed7ecc
danza0057 <danza0057@yandex.ru>
2021-10-04 16:53:44 +0300

    Update

--- main_1.py
+++
@@ -1,3 +0,0 @@
-my_list = [23, 34.56, "str", True, None, [2, 3]]
-for el in my_list:
-    print(type(el))
--- main_4.py
+++
@@ -1,3 +0,0 @@
-my_list = input('Введите строку: ').split()
-for i, el in enumerate(my_list):
-    print(i, el[:10])
